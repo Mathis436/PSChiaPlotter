c76b734c2451c921bda98b911659bbb7b9bd1a6e
Mathis436 <67963297+Mathis436@users.noreply.github.com>
2021-12-03 11:27:27 -0600

    Add files via upload

---
+++ PSChiaPlotter/Class/K30.ps1
@@ -0,0 +1,33 @@
+class OptimizedKPlots {
+    [int]$K35
+    [int]$K34
+    [int]$K33
+    [int]$K31
+    [int]$K30
+    [int]$K29
+    [int]$K32
+    [decimal]$RemainingBytes
+    [double]$RemainingGB
+
+    OptimizedKPlots (
+        [int]$K35,
+        [int]$K34,
+        [int]$K33,
+        [int]$K31,
+        [int]$K30,
+        [int]$K29,
+        [int64]$Totalbytes
+    ){
+        $sizeremaining = $TotalBytes - (($K35 * [MaximizedKSize]::K35) + ($K34 * [MaximizedKSize]::K34) + ($K33 * [MaximizedKSize]::K33))
+        $k32max = Get-MaxKSize -Totalbytes $sizeremaining -KSize "K32"
+        $this.K35 = $K35
+        $this.K34 = $K34
+        $this.K33 = $K33
+        $this.K30 = $K31
+        $this.K30 = $K30
+        $this.K30 = $K29
+        $this.K32 = $k32max.MaxPlots
+        $this.RemainingBytes = $k32max.RemainingBytes
+        $this.RemainingGB = [math]::Round($k32max.RemainingBytes / 1gb,2)
+    }
+}
--- PSChiaPlotter/Class/MaximizedKSize.ps1
+++ PSChiaPlotter/Class/MaximizedKSize.ps1
@@ -1,4 +1,7 @@
 enum KSize{
+    K29 = 29
+    K30 = 30
+    K31 = 31
     K32 = 32
     K33 = 33
     K34 = 34
@@ -16,6 +19,9 @@ class MaximizedKSize {
     static [Decimal]$K34 = 429.8 * 1gb
     static [Decimal]$K33 = 208.8 * 1gb
     static [Decimal]$K32 = 101.4 * 1gb
+    static [Decimal]$K31 = 50.7 * 1gb
+    static [Decimal]$K30 = 23.9 * 1gb
+    static [Decimal]$K29 = 11.8 * 1gb
 
     MaximizedKSize(
         [KSize]$KSize,
@@ -29,6 +35,9 @@ class MaximizedKSize {
             "K34" {[MaximizedKSize]::K34}
             "K33" {[MaximizedKSize]::K33}
             "K32" {[MaximizedKSize]::K32}
+            "K31" {[MaximizedKSize]::K31}
+            "K30" {[MaximizedKSize]::K30}
+            "K29" {[MaximizedKSize]::K29}
         }
         $this.MaxPlots = [math]::Floor([decimal]($this.TotalBytes / $this.KSizeBytes))
         $this.RemainingBytes = $Totalbytes - (([math]::Floor([decimal]($this.TotalBytes / $this.KSizeBytes))) * $this.KSizeBytes)
